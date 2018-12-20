ddccd4820e517ccb1bc3816a3d9c1eba2a927c33
Iceyer <me@iceyer.net>
2018-12-20 13:10:31 +0800

    fix: update redirect uri
    Change-Id: Ia43659a343cc46f2474c25e7c4e043f34a2843b0

--- src/login_window.cpp
+++ src/login_window.cpp
@@ -28,7 +28,7 @@ public:
         QString templateURL = "%1/oauth2/authorize?client_id=%2&redirect_uri=%3&response_type=code&scope=%4&display=sync&handle_open_link=true";
         QString oauthURI = "https://login.deepin.org";
         QString clientID = "163296859db7ff8d72010e715ac06bdf6a2a6f87";
-        QString redirectURI = "https://sync.deepin.com/oauth/callback";
+        QString redirectURI = "https://sync.deepinid.deepin.com/oauth/callback";
         QString scope = "base,user:read,sync,dstore";
 
         if (!qEnvironmentVariableIsEmpty("DEEPIN_DEEPINID_OAUTH_URI")) {
