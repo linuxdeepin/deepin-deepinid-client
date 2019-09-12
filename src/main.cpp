6f0b658ff895051ed12e59a6bc2f1f2d44268de7
Iceyer <me@iceyer.net>
2019-09-12 10:14:50 +0800

    remove libqcef
    Change-Id: I2fa84de82ccff96c9a3f8b6cce76bd5eb6b846c9

--- src/main.cpp
+++ src/main.cpp
@@ -20,56 +20,10 @@ const char DBusService[] = "com.deepin.deepinid.Client";
 const char DBusPath[] = "/com/deepin/deepinid/Client";
 } // namespace Const
 
-//bool initQCef(int argc, char **argv)
-//{
-//    QCefGlobalSettings settings;
-//    // Do not use sandbox.
-//    settings.setNoSandbox(true);
-
-//    if (qEnvironmentVariableIntValue("QCEF_DEBUG") == 12) {
-//        // Open http://localhost:9222 in chromium browser to see dev tools.
-//        settings.setRemoteDebug(true);
-//        settings.setLogSeverity(QCefGlobalSettings::LogSeverity::Verbose);
-//    } else {
-//        settings.setRemoteDebug(false);
-//        settings.setLogSeverity(QCefGlobalSettings::LogSeverity::Error);
-//    }
-
-//    // Disable GPU process.
-//    settings.addCommandLineSwitch(Const::DisableGpu, "");
-
-//    // Enable aggressive storage commit to minimize data loss.
-//    // See public/common/content_switches.cc.
-//    settings.addCommandLineSwitch(Const::EnableDomStorageFlush, "");
-
-//    QDir cacheDir(Dtk::Core::DStandardPaths::standardLocations(QStandardPaths::CacheLocation).value(0));
-//    QDir appCacheDir(cacheDir.absoluteFilePath("deepin/deepin-sync-client"));
-//    appCacheDir.mkpath(".");
-
-//    settings.setCachePath(appCacheDir.filePath("cache"));
-//    settings.setUserDataPath(appCacheDir.filePath("cef-storage"));
-
-//    settings.setLogFile(appCacheDir.filePath("web-console.log"));
-//    settings.addCommandLineSwitch(Const::EnableLogging, "");
-//    settings.addCommandLineSwitch(Const::LogLevel, "0");
-
-//    settings.setBackgroundColor(0x00FFFFFF);
-//    auto ret = QCefInit(argc, argv, settings);
-//    if (ret >= 0) {
-//        qCritical() << "init qcef failed" << ret;
-//        return false;
-//    }
-//    return true;
-//}
-
 int main(int argc, char **argv)
 {
     qputenv("DXCB_FAKE_PLATFORM_NAME_XCB", "true");
     qputenv("DXCB_REDIRECT_CONTENT", "true");
-
-//    if (!initQCef(argc, argv)) {
-//        return -1;
-//    }
 
     Dtk::Core::DLogManager::registerConsoleAppender();
     Dtk::Core::DLogManager::registerFileAppender();
