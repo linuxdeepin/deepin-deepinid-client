b16fae77c3f8a642b4874bfbcc278a428daa8e47
Iceyer <me@iceyer.net>
2019-11-28 09:16:12 +0800

    fix: update translations
    Change-Id: I0e96eee4adf28842879edf21f1e7fc70a1018c03

--- translations/deepin-deepinid-client_zh_CN.ts
+++ translations/deepin-deepinid-client_zh_CN.ts
@@ -4,32 +4,32 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/sync_client.cpp" line="29"/>
+        <location filename="../src/ui/sync_client.cpp" line="23"/>
         <source>View</source>
         <translation>查看</translation>
     </message>
     <message>
-        <location filename="../src/sync_client.cpp" line="89"/>
+        <location filename="../src/ui/sync_client.cpp" line="117"/>
         <source>Deepin ID Privacy Policy</source>
         <translation>Deepin ID隐私政策</translation>
     </message>
     <message>
-        <location filename="../src/sync_client.cpp" line="90"/>
+        <location filename="../src/ui/sync_client.cpp" line="118"/>
         <source>Agree and Turn On Cloud Sync</source>
         <translation>同意并开启云同步功能</translation>
     </message>
 </context>
 <context>
-    <name>dsc::SyncClient</name>
+    <name>ddc::SyncClient</name>
     <message>
-        <location filename="../src/sync_client.cpp" line="84"/>
-        <location filename="../src/sync_client.cpp" line="102"/>
-        <location filename="../src/sync_client.cpp" line="168"/>
+        <location filename="../src/ui/sync_client.cpp" line="133"/>
+        <location filename="../src/ui/sync_client.cpp" line="191"/>
+        <location filename="../src/ui/sync_client.cpp" line="204"/>
         <source>Login failed</source>
         <translation>登录失败</translation>
     </message>
     <message>
-        <location filename="../src/sync_client.cpp" line="170"/>
+        <location filename="../src/ui/sync_client.cpp" line="199"/>
         <source>Login successful, please go to Cloud Sync to view the settings</source>
         <translation>Deepin ID登录成功，可前往云同步模块查看设置选项</translation>
     </message>
