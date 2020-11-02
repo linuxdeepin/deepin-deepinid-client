f3733abd222be689085ab8b3f9d20732b645024e
lichengfeng <lichengfeng@uniontech.com>
2020-11-02 16:02:02 +0800

    chore: deprecated静态成员函数删除
    Description: loadDXcbPlugin被弃用，相关代码进行删除
    
    Log: 删除无用代码

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,6 @@ const char DBusPath[] = "/com/deepin/deepinid/Client";
 int main(int argc, char **argv)
 {
     qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--single-process");
-    Dtk::Widget::DApplication::loadDXcbPlugin();
     Dtk::Widget::DApplication app(argc, argv);
 
     app.setAttribute(Qt::AA_ForceRasterWidgets, false);
