9618053263220658956d2347e7e3456deb6ce1a5
duanhongyu <duanhongyu@uniontech.com>
2022-04-29 10:13:35 +0800

    Revert "feat: 修改社区版弹窗大小"
    This reverts commit 9ec52033f9f81b190b1655ece233b14fc9135de0.
    
    Reason for revert: <INSERT REASONING HERE>
    废弃需求
    
    Change-Id: I402561f78bb585952cbd4b9bdd42834d467e327a

--- src/ui/login_window.cpp
+++ src/ui/login_window.cpp
@@ -257,7 +257,7 @@ LoginWindow::LoginWindow(QWidget *parent)
 
     connect(this, &LoginWindow::loadError, this, &LoginWindow::onLoadError, Qt::QueuedConnection);
 
-    setFixedSize(380, 550 + this->titlebar()->height());
+    setFixedSize(360, 390 + this->titlebar()->height());
     QTimer::singleShot(100, this, SLOT(setFocus()));
 }
 
